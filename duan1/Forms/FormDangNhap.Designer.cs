75040597a09d0a4a712db45554277131cbef4af5
Rin0050 <bodoino111112@gmail.com>
2025-08-07 18:04:10 +0700

    quan ly voucher

--- duan1/Forms/FormDangNhap.Designer.cs
+++ duan1/Forms/FormDangNhap.Designer.cs
@@ -28,60 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDangNhap));
             txtEmail = new TextBox();
             txtMatKhau = new TextBox();
             btnDangNhap = new Button();
             label1 = new Label();
             label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(313, 218);
+            txtEmail.Location = new Point(325, 335);
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new Size(125, 27);
             txtEmail.TabIndex = 0;
             // 
             // txtMatKhau
             // 
-            txtMatKhau.Location = new Point(314, 269);
+            txtMatKhau.Location = new Point(325, 399);
             txtMatKhau.Name = "txtMatKhau";
             txtMatKhau.Size = new Size(125, 27);
             txtMatKhau.TabIndex = 1;
             // 
             // btnDangNhap
             // 
-            btnDangNhap.Location = new Point(325, 319);
+            btnDangNhap.Location = new Point(342, 462);
             btnDangNhap.Name = "btnDangNhap";
             btnDangNhap.Size = new Size(94, 29);
             btnDangNhap.TabIndex = 2;
-            btnDangNhap.Text = "Dang Nhap";
+            btnDangNhap.Text = "Đăng nhập";
             btnDangNhap.UseVisualStyleBackColor = true;
             btnDangNhap.Click += btnDangNhap_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(224, 226);
+            label1.Location = new Point(227, 338);
             label1.Name = "label1";
-            label1.Size = new Size(46, 20);
+            label1.Size = new Size(49, 20);
             label1.TabIndex = 3;
-            label1.Text = "Email";
+            label1.Text = "Email:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(227, 278);
+            label2.Location = new Point(227, 402);
             label2.Name = "label2";
-            label2.Size = new Size(70, 20);
+            label2.Size = new Size(73, 20);
             label2.TabIndex = 4;
-            label2.Text = "Mat khau";
+            label2.Text = "Mật khẩu:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(203, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(340, 293);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
             // 
             // FormDangNhap
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(759, 512);
+            Controls.Add(pictureBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(btnDangNhap);
@@ -89,6 +103,7 @@
             Controls.Add(txtEmail);
             Name = "FormDangNhap";
             Text = "FormDangNhap";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -100,5 +115,6 @@
         private Button btnDangNhap;
         private Label label1;
         private Label label2;
+        private PictureBox pictureBox1;
     }
 }
